48a56e7196ac5f77597531e82d92d079c3887975
roche <email": null>
2016-03-10 18:18:27 -0800

    add relationships to models

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,10 @@ class Tag < ActiveRecord::Base
   belongs_to :user
   has_many :taggings
   has_many :resumes, through: :taggings, source: :taggable, source_type: "Resume"
+  has_many :experiences, through: :taggings, source: :taggable, source_type: "Experience"
+  has_many :objectives, through: :taggings, source: :taggable, source_type: "Objective"
+  has_many :skills, through: :taggings, source: :taggable, source_type: "Skill"
+  has_many :volunteerings, through: :taggings, source: :taggable, source_type: "Volunteering"
+  has_many :projects, through: :taggings, source: :taggable, source_type: "Project"
+  has_many :educations, through: :taggings, source: :taggable, source_type: "Education"
 end

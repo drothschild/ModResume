394221a1f71f5ed58e01894d74b968a6cae1880e
roche <email": null>
2016-03-11 17:09:24 -0800

    Add beginning of list all page

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,5 +7,7 @@ class AssetsController < ApplicationController
     @projects = current_user.projects
     @educations = current_user.educations
     @tags = current_user.tags
+    # @assets = {Objectives: @objectives, Skills: @skills, Volunteer: @volunteerings, Experience: @experiences, Projects: @projects}
+    @assets = {"educations" => @educations}
   end
 end

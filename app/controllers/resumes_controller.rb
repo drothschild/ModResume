ad5821330a9c89e7737023c9619d26cba557e952
[01;31m[K"name"[m[K: "Gary Wong <[01;31m[K"email"[m[K: null>
2016-03-11 20:38:12 -0800

    Add link to destroy resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,7 @@
 class ResumesController < ApplicationController
-
+  before_action :set_resume, only: [:edit, :show, :update, :destroy]
+  
   def index
-    # @resumes = Resume.all
     if current_user
       @resumes = Resume.where(user_id: current_user.id)
     else
@@ -11,10 +11,31 @@ class ResumesController < ApplicationController
   end
 
   def show
-    @resume = Resume.find(params[:id])
-    @list = ["Red","Green","Blue","Yellow","Black","White","Orange"]
     # @resume = Resume.create(target_job: "Rockstar", document_data: "Who runs the world? Squirrels!")
     # render json: @resume
+  end
+
+  def new
+    @tags = current_user.tags
+    @resume = Resume.new()
+  end
+
+  def create
+  end
+
+  # edit html text
+  def edit
+    # @resume = Resume.find(params[:id])
+  end
+
+  def destroy
+    @remove.destroy
+    redirect_to user_resumes_path(params[:user_id])
+  end
+
+private
+  def set_resume
+    @resume = Resume.find(params[:id])
   end
 
 end

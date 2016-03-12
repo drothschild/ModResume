29ca8211547bd0f51f0569c07394ed405242636f
roche <email": null>
2016-03-11 22:10:02 -0800

    Add resume show page

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -7,7 +7,9 @@ class ResumesController < ApplicationController
 
   def show
     @resume = Resume.find(params[:id])
-    @list = ["Red","Green","Blue","Yellow","Black","White","Orange"]
+    p @resume_assets
+    @resume_assets = ResumeAsset.where("resume_id = ?", params[:id])
+    # @list = ["Red","Green","Blue","Yellow","Black","White","Orange"]
     # @resume = Resume.create(target_job: "Rockstar", document_data: "Who runs the world? Squirrels!")
     # render json: @resume
   end

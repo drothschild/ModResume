2b949c61ad0aaa58f8e3ef8a25740e3825719796
Lindsey Ullman <lindsey.ullman@gmail.com>
2016-03-13 13:33:03 -0700

    Update asset filtering

--- app/assets/javascripts/assets.js
+++ app/assets/javascripts/assets.js
@@ -11,8 +11,29 @@ var bindAssetListeners = function() {
 var toggleTag = function(e) {
   e.preventDefault();
   console.log("Tag Button Clicked");
-  console.log(this);
+  // console.log(this);
   var tagID = $(this).attr("tag_id");
+  // console.log(tagID);
+  $(this).toggleClass("tag-button-selected");
+  var visible = $(this).hasClass("tag-button-selected");
+  toggleAssets(tagID, visible);
+}
+
+var toggleAssets = function(tagID, visible) {
   console.log(tagID);
-  $(this).toggleClass("tag-button-selected")
+  var assetDivs = $(".asset-container");
+  // console.log("Asset Divs:");
+  // console.log(assetDivs);
+  for (var i = 0; i < assetDivs.length; i++) {
+    // console.log($(assetDivs[i]).attr("asset_id"));
+    // console.log($(assetDivs[i]).attr("tags"));
+    if ($(assetDivs[i]).attr("tags").includes("|" + tagID + "|") == true) {
+      if (visible === true) {
+        $(assetDivs[i]).show();
+      }
+      else {
+        $(assetDivs[i]).hide();
+      }
+    }
+  }
 }

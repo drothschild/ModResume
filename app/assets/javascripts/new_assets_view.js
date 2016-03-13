1a69261d4a50bc9e06ec18709a53e43f55ea7414
roche <email": null>
2016-03-12 16:53:48 -0800

    Parse tag value from form

--- app/assets/javascripts/new_assets_view.js
+++ app/assets/javascripts/new_assets_view.js
@@ -5,18 +5,31 @@ newAssetsView.prototype.loadForm = function(event){
   $.ajax({url: uri, method: "GET"}).done(function(response){
     $('#form-container').html(response);
   })
+}
 
+newAssetsView.prototype.saveForm = function(event){
+  console.log($(this))
+  var data = $('form').serialize()
+  var tags = $('input#tags')[0].value
+  var uri = event.target.action
+  $.ajax({url: uri, method: "POST", data: data}).done(function(response){
+    console.log(response)
+  })
 }
 
 var newAssets = new newAssetsView();
 $(document).ready(function(){
-  console.log("new assets view here!")
   $('.asset-type-button').on("click", function(e){
     e.preventDefault();
     $(e.target.parentNode.children).removeClass("active");
     $(e.target).addClass("active");
     $('#form-container').html("");
     newAssets.loadForm(e);
+  })
+  $('#form-container').on("submit", 'form', function(e){
+    e.preventDefault();
+    //load a saved div
+    newAssets.saveForm(e);
   })
 
 })

4858ef5e3ef558e122dc88d16d737bae67dec702
roche <email": null>
2016-03-13 19:57:31 -0700

    Add two mediocre pdf options

--- app/assets/javascripts/resume_sort_view.js
+++ app/assets/javascripts/resume_sort_view.js
@@ -18,13 +18,47 @@ $(document).ready(function() {
   console.log("hello")
 });
 
-// $(function() {
-//   $('.section').sortable({
-//     connectWith: ".section",
-//     handle: '.section-header',
-//     placeholder: "section-placeholder ui-corner-all"
+// JSPDF JSPDF JSPDF JSPDF
 
-//   })
-//   $('.asset-type').sortable();
-//   console.log("hello")
-// });
+function resumeFromHTML(){
+  var pdf = new jsPDF('p', 'pt', 'letter');
+  source = $('.resume-template')[0];
+
+  specialElementHandlers = {
+    '#skipme': function (element, renderer){
+      return true
+    }
+  };
+
+  margins = {
+    top: 80,
+    bottom: 60,
+    left: 40,
+    width: 522
+  }
+
+  pdf.fromHTML(
+    source,
+    margins.left,
+    margins.top, {
+      'width': margins.width,
+      'elementHandlers': specialElementHandlers
+  },
+
+  function(dispose){
+    pdf.save('Resume.pdf')
+  }, margins );
+
+}
+// END JSPDF JSPDF JSPDF JSPDF
+
+function printDiv(){
+  console.log($('.resume-template').html());
+  var printContents = $('.resume-template').html();
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+
+}

352d5465779e7e1949d2080ade00c5a8eb45b0fa
roche <email": null>
2016-03-15 10:06:27 -0700

    Add fine tune route to controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       }
 
   resources :users, except: [:index, :destroy] do
-    resources :websites, except: [:index, :show]
+    resources :websites, except: [:index]
     resources :tags
 
     resources :assets, only: [:index, :new]
@@ -17,16 +17,17 @@ Rails.application.routes.draw do
     resources :projects, except: [:index]
     resources :skills, except: [:index]
     resources :volunteerings, except: [:index]
-    resources :taggings, except: [:index, :show]
-    resources :resume_assets, except: [:index, :show]
-    # resources :resume_print
+    resources :taggings, except: [:index]
+    resources :resume_assets, except: [:index]
+    resources :resume_print
     resources :resumes
 
   end
 
 
-
   get':controller(/:action(/:id))'
+
+  post 'users/:id/resumes/:id/fine-tune', to: 'resumes#fine_tune', as: 'user_resume_fine_tune'
 
   root 'root#index'
 end

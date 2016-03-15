ac8b4429f831abf3a1896beba3b7007c024c5f98
roche <email": null>
2016-03-15 11:05:28 -0700

    Add AWESOME TinyMCE fine-tune feature

--- app/assets/javascripts/fine_tune.js
+++ app/assets/javascripts/fine_tune.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
   $("#fine-tune-button").on("click", function(e){
     e.preventDefault();
     var data = $('.resume-template').html()
-    console.log(data)
     var uri = $(this).attr("href")
-    $.ajax({url: uri, method: "POST", data: {data:data}}).done(function(response){
-      console.log(response)
+    $.ajax({url: uri, method: "POST", data: {document_data: data}}).done(function(response){
+      window.location.href = "localhost:3000/users/62/resumes/101/fine-tune"
+      $('.sortable-wrapper').html(response)
+      setTimeout(function(){
+        tinyMCE.activeEditor.setContent(data)
+      }, 1000)
     })
   })
 

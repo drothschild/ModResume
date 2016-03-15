961f7a125284ba2510d16fc0e7d8b5491d6d6992
roche <email": null>
2016-03-15 08:47:16 -0700

    Restrict user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         registrations: 'users/registrations'
       }
 
-  resources :users do
+  resources :users, except: [:index, :destroy] do
     resources :websites, except: [:index, :show]
     resources :tags
 

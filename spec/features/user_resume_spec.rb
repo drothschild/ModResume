f3a05479be644d6372370f647a2bb30812383172
[01;31m[K"name"[m[K: "Gary Wong <[01;31m[K"email"[m[K: null>
2016-03-14 20:52:18 -0700

    Add test to navigate to edit asset page

--- spec/features/user_resume_spec.rb
+++ spec/features/user_resume_spec.rb
@@ -6,11 +6,14 @@ RSpec.feature "Resumes", type: :feature, js: true do
     @resume = FactoryGirl.create(:resume)
     @experience = FactoryGirl.create(:experience)
     @education = FactoryGirl.create(:education)
+    @education.save!
+    @experience.save!
     @user.save!
     @user.resumes << @resume
     ResumeAsset.create(resume_id: @resume.id, buildable_id: @experience.id, buildable_type: 'Experience')
     ResumeAsset.create(resume_id: @resume.id, buildable_id: @education.id, buildable_type: 'Education')
-
+    @user.educations << @education
+    @user.experiences << @experience
     @resume.save!
   end
 
@@ -59,4 +62,24 @@ RSpec.feature "Resumes", type: :feature, js: true do
       draggable_item.drag_to(target)
     end
 
+    it 'should be able to navigate to edit asset page' do
+      visit '/'
+      fill_in "user_email", :with => @user.email
+      fill_in "user_password", :with => 'password'
+      click_button "Log in"
+      first(:link, @resume.target_job).click
+      visit edit_user_resume_path(@user.id, @resume.id)
+      expect(page).to have_content('Experience')
+      expect(page).to have_content('Objective')
+    end
+
+    it 'should be able to edit asset page' do
+      visit '/'
+      fill_in "user_email", :with => @user.email
+      fill_in "user_password", :with => 'password'
+      click_button "Log in"
+      first(:link, @resume.target_job).click
+      visit edit_user_resume_path(@user.id, @resume.id)
+      binding.pry
+    end
 end

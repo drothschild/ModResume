966c035bbc0c0c22d951bfb47e91d3856203521a
roche <email": null>
2016-03-16 10:20:03 -0700

    Set session current_resume_id on click and add asset_type_grammar

--- app/assets/javascripts/print_resume.js
+++ app/assets/javascripts/print_resume.js
@@ -3,6 +3,7 @@ var printResume = function(e) {
   var backup = $("body").html();
   var doc = $(".resume-template").html();
   $("body").css("margin", "25mm 25mm 25mm 25mm");
+  $("body").css("border-color", "white")
   $("body").html(doc);
   window.print();
   $("body").html(backup);

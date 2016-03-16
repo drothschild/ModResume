e88bd12d14f133269cfa1febf20e2bc18cd299ea
roche <email": null>
2016-03-16 14:54:54 -0700

    Test on local

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -1,9 +1,8 @@
 module PdfHelper
 
   def self.parse_pdf(reader, user)
-    p reader.info
     @skill = Skill.new(title: "I can parse pdfs!")
-    git auser.skills << @skill
+    user.skills << @skill
   end
 
 

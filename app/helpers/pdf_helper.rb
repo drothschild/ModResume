820a29ccddbf98834d533e377fb2fbcf7ab5a8b5
roche <email": null>
2016-03-16 19:08:40 -0700

    Test with several different pdfs

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -16,65 +16,73 @@ module PdfHelper
     skills = []
     education = []
 
-
     i = 0
     # skip heading
-    while all_lines[i] != "Summary" do
-      i += 1
+    if all_lines.include?("Summary")
+      while all_lines[i] != "Summary" do
+        i += 1
+      end
     end
 
-    # skip the word "Summary"
-    i += 1
-
-    # get summary
-    while all_lines[i] != "Experience" do
-      summary << all_lines[i]
+      # skip the word "Summary"
       i += 1
+
+      # get summary
+    if all_lines.include?("Experience")
+      while all_lines[i] != "Experience" do
+        summary << all_lines[i]
+        i += 1
+      end
     end
 
-    # skip the word "Experience"
-    i += 1
-
-    # get experience
-    while all_lines[i] != "Skills & Expertise"
-      experience << all_lines[i]
+      # skip the word "Experience"
       i += 1
+
+      # get experience
+    if all_lines.include?("Skills & Expertise")
+      while all_lines[i] != "Skills & Expertise"
+        experience << all_lines[i]
+        i += 1
+      end
     end
 
-    # skip the word "Skills & Expertise"
-    i += 1
-
-    # get skills
-    while all_lines[i] != "Education"
-      skills << all_lines[i]
+      # skip the word "Skills & Expertise"
       i += 1
+
+      # get skills
+    if all_lines.include?("Education")
+      while all_lines[i] != "Education"
+        skills << all_lines[i]
+        i += 1
+      end
     end
 
-    # skip the word "Education"
-    i += 1
-
-    # get education
-    while all_lines[i] != "Courses"
-      education << all_lines[i]
+      # skip the word "Education"
       i += 1
+
+      # get education
+    if all_lines.include?("Courses")
+      while all_lines[i] != "Courses"
+        education << all_lines[i]
+        i += 1
+      end
     end
 
     make_objective(summary, user)
     make_skills(skills, user)
     make_experiences(experience, user)
-    make_education(education, user)
+    # make_education(education, user)
   end
 
-  def self.make_education(array, user)
-    education_sections = array.each_slice(2).to_a
-    education_sections.each do |subarray|
-      study_dates = subarray.split(",")
-      # completion = "January " + study_dates[3].split("").last(4).join
+  # def self.make_education(array, user)
+  #   education_sections = array.each_slice(2).to_a
+  #   education_sections.each do |subarray|
+  #     study_dates = subarray.split(",")
+  #     # completion = "January " + study_dates[3].split("").last(4).join
 
-      user.educations << Education.create(institution_name: subarray[0], focus: study_dates[0], completion: Date.today)
-    end
-
-  end
+  #     user.educations << Education.create(institution_name: subarray[0], focus: study_dates[0], completion: Date.today)
+  #   end
+  # end
 
   def self.make_dates(string)
     return Date.today if string == nil

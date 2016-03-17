6b125526fd766fdeda663897ddaaa8dd659c8a48
David Rothschild <david@rothschildsolutions.com>
2016-03-17 11:18:19 -0700

    refactor models with concerns

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -5,4 +5,15 @@ module Taggable
     has_many :tags, through: :taggings
     has_many :taggings, as: :taggable
   end
+
+  
+
+  def tags_string=(tags_string)
+    tag_names = tags_string.split(',')
+    tag_names.each do |word|
+      word = word.strip
+      tag =Tag.find_or_create_by(name: word, user: current_user)
+      tags << tag
+    end
+  end
 end
